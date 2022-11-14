96e7f5a666e709d2fb6ece89ac3f77a463a2dde4
Wendy Teo <wendyteo.wy@gmail.com>
2022-11-15 06:31:36 +1100

    Fetched date to customer component

--- src/contexts/DbContext.tsx
+++ src/contexts/DbContext.tsx
@@ -1,6 +1,6 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import { getFirestore, collection, onSnapshot } from 'firebase/firestore';
-import { billingAddress } from '../types/BillingAddress';
+import { billingAddress, defaultState } from '../types/BillingAddress';
 
 const DbContext = React.createContext<any>(null);
 
@@ -16,14 +16,14 @@ export function DbProvider(props: { children: React.ReactNode }) {
     const billAddressColRef = collection(db, 'billingAddresses');
     
     // Data collected from Firestore
-    const billingAddresses: billingAddress[] = [];
-    
-    onSnapshot(billAddressColRef, (snapshot) => {
-        snapshot.docs.forEach((doc: any) => {
-            billingAddresses.push({ ...doc.data() })
-        })
-        console.log(billingAddresses);
-    });
+    const [billingAddresses, setBillingAddresses] = useState<billingAddress[]>([ defaultState ]);
+
+    useEffect(() => {
+        onSnapshot(billAddressColRef, (snapshot) =>{
+            console.log(snapshot.docs.map((doc: any) => { return {...doc.data(), id: doc.id} }));
+            setBillingAddresses(snapshot.docs.map((doc: any) => { return {...doc.data(), id: doc.id} }));
+        });
+    }, []);
 
     const value = {
         billAddressColRef,
