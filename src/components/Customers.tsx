5dd7b73c0992befa71e24e88a2be5b6be49af481
Wendy Teo <wendyteo.wy@gmail.com>
2022-11-16 05:47:51 +1100

    Replaced list with a table

--- src/components/Customers.tsx
+++ src/components/Customers.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { billingAddress } from 'src/types/BillingAddress';
 import { useDb } from '../contexts/DbContext';
 
 
@@ -10,12 +11,33 @@ function Customers() {
     
 
     return (
-        <ul className={styles.customersList}>
-            { billingAddresses.map((billingAddress: any) => (
-                <li key={ billingAddress.id} >{ billingAddress.company } - { billingAddress.billedTo }</li>
-                ))
+        <table className={styles.customersList}>
+            <thead>
+                <tr>
+                    <th>Billed To</th>
+                    <th>Company</th>
+                    <th>Amount</th>
+                    <th>Status</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+            <tbody>
+            {
+                billingAddresses.map((billingAddress: billingAddress) => (
+                    <tr key={billingAddress.company}>
+                        <td>{billingAddress.billedTo}</td>
+                        <td>{billingAddress.company}</td>
+                        <td></td>
+                        <td></td>
+                        <td>
+                            <button>Delete</button>
+                            <button>Update</button>
+                        </td>
+                    </tr>
+                )) 
             }
-        </ul>);
+            </tbody>
+        </table>);
 }
 
 export default Customers;
