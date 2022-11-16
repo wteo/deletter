9a08265dcf48d011e7b6ae992130e5323a2d0866
Wendy Teo <wendyteo.wy@gmail.com>
2022-11-17 08:27:34 +1100

    Added invoice collection

--- src/contexts/DbContext.tsx
+++ src/contexts/DbContext.tsx
@@ -1,6 +1,7 @@
 import React, { useContext, useState, useEffect } from 'react';
 import { getFirestore, collection, onSnapshot } from 'firebase/firestore';
-import { billingAddress, defaultState } from '../types/BillingAddress';
+import { billingAddress, billingAddressDefaultState } from '../types/BillingAddress';
+import { invoice, invoiceDefaultState } from '../types/Invoice';
 
 const DbContext = React.createContext<any>(null);
 
@@ -14,21 +15,31 @@ export function DbProvider(props: { children: React.ReactNode }) {
     
     // collection refs
     const billAddressColRef = collection(db, 'billingAddresses');
+    const invoiceColRef = collection(db, 'invoices');
     
     // Data collected from Firestore
-    const [billingAddresses, setBillingAddresses] = useState<billingAddress[]>([ defaultState ]);
+    const [billingAddresses, setBillingAddresses] = useState<billingAddress[]>([ billingAddressDefaultState ]);
+    const [invoices, setInvoices] = useState<invoice[]>([ invoiceDefaultState ]);
 
     useEffect(() => {
+        // Fetching billingAddress
         onSnapshot(billAddressColRef, (snapshot) =>{
             console.log(snapshot.docs.map((doc: any) => { return {...doc.data(), id: doc.id} }));
             setBillingAddresses(snapshot.docs.map((doc: any) => { return {...doc.data(), id: doc.id} }));
+        });
+
+        // Fetching invoices
+        onSnapshot(invoiceColRef, (snapshot) =>{
+            console.log(snapshot.docs.map((doc: any) => { return {...doc.data(), id: doc.id} }));
+            setInvoices(snapshot.docs.map((doc: any) => { return {...doc.data(), id: doc.id} }));
         });
     }, []);
 
     const value = {
         db,
         billAddressColRef,
-        billingAddresses
+        billingAddresses,
+        invoices
     };
 
     return (
