3dbe243a95cce46dfd2d164c7861201a72c5af46
Wendy Teo <wendyteo.wy@gmail.com>
2022-11-17 09:06:36 +1100

    change type

--- src/types/Invoice.tsx
+++ src/types/Invoice.tsx
@@ -1,19 +1,19 @@
 export type invoice = {
     docNo: string,
     docType: string, 
-    date: number | string,
-    month: number | string,
-    year: number | string,
-    cost: number | string,
+    date: number | null,
+    month: number | null,
+    year: number | null,
+    cost: number | null,
     tax: boolean
 };
 
 export const invoiceDefaultState: invoice = {
     docNo: '',
     docType: '', 
-    date: '',
-    month: '',
-    year: '',
-    cost: '',
+    date: null,
+    month: null,
+    year: null,
+    cost: null,
     tax: true
 };
