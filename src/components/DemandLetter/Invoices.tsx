25235925fbeb9c50ebb8a7aff4da19e3cf054547
Wendy Teo <wendyteo.wy@gmail.com>
2022-11-17 07:04:23 +1100

    Added form to invoice

--- src/components/DemandLetter/Invoices.tsx
+++ src/components/DemandLetter/Invoices.tsx
@@ -17,15 +17,16 @@ function Invoices() {
     }]
 
     return (
+        <>
         <table id={styles.invoicesList}>
             <thead>
                 <tr>
                     <th>Document no</th>
                     <th>Document type</th>
                     <th>Billed date</th>
-                    <th>Amount</th>
+                    <th>Cost</th>
                     <th>GST</th>
-                    <th>Billed</th>
+                    <th>Total</th>
                     <th>Action</th>
                 </tr>
             </thead>
@@ -41,7 +42,7 @@ function Invoices() {
                             <td>{(invoice.cost + invoice.cost*0.1).toFixed(2)}</td>
                             <td>
                                 <form>
-                                    <input type="button" value="delete" />
+                                    <input type="button" value="Delete" />
                                 </form>
                             </td>
                         </tr>
@@ -49,6 +50,36 @@ function Invoices() {
                 }
             </tbody>
         </table>
+        <form id={styles.invoiceForm}>
+            <label>Document no:</label>
+            <input type="text" />
+            <br/>
+            <label>Document Type:</label>
+            <select id={styles.invoices}>
+                <option value="invoice">Tax Invoice</option>
+                <option value="credit">Credit Note</option>
+                <option value="overpayment">Overpayment</option>
+            </select>
+            <br/>
+            <label>Billed Date (in MM/DD/YY format):</label>
+            <div>
+                <input type="text" />
+                <input type="text" />
+                <input type="text" />
+            </div>
+            <br/>
+            <label>Cost:</label>
+            <input type="text" />
+            <br/>
+            <label>Tax:</label>
+            <select id={styles.gst}>
+                <option value="gst">GST 10%</option>
+                <option value="noGST">No Tax</option>
+            </select>
+            <br/>
+            <button>Submit</button>
+        </form>
+        </>
     );
 }
 
