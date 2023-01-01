77db177c7f0bf94e329036362203008d1c42f755
Wendy Teo <wendyteo.wy@gmail.com>
2023-01-02 09:22:26 +1100

    sorted inv list

--- src/components/DemandLetter/Invoices/Invoices.tsx
+++ src/components/DemandLetter/Invoices/Invoices.tsx
@@ -20,8 +20,18 @@ function Invoices() {
         deleteDoc(docRef);
     };
 
-    type docNo = { docNo: number | string };
-    const sortInvoices = invoices.sort((a: docNo , b: docNo) => (a.docNo > b.docNo) ? 1 : ((b.docNo > a.docNo) ? -1 : 0))
+    // type docNo = { docNo: number | string };
+    type customerName = { customerName: string };
+    // type cost = { cost: number};
+    
+    // Sorting invoices
+    // const sortInvsByDocNo = invoices.sort((a: docNo , b: docNo) => 
+    //    (a.docNo > b.docNo) ? 1 : ((b.docNo > a.docNo) ? -1 : 0));
+    const sortInvsByCustomerName = invoices.sort((a: customerName , b: customerName) => 
+        (a.customerName > b.customerName) ? 1 : ((b.customerName > a.customerName) ? -1 : 0));
+    // const sortInvsByCost = invoices.sort((a: cost, b: cost) => (b.cost) - (a.cost));
+    
+    // Formatting nums
     const formatNum = (num: number) => num.toLocaleString(undefined, { minimumFractionDigits: 2, maximumFractionDigits: 2 });
 
     return (
@@ -41,7 +51,7 @@ function Invoices() {
             </thead>
             <tbody>
                 {
-                    sortInvoices.map((invoice: invoice) => {
+                    sortInvsByCustomerName.map((invoice: invoice) => {
                         const cost = formatNum(Number(invoice.cost));
                         const gst = formatNum(Number(invoice.cost)*0.1);
                         const total = formatNum(Number(invoice.cost) + Number(invoice.cost)*0.1);
@@ -52,15 +62,17 @@ function Invoices() {
                                 <td>{invoice.docType}</td>
                                 <td>{invoice.date}/{invoice.month}/{invoice.year}</td>
                                 <td>{cost}</td>
-                                { Number(invoice.tax) === 1 ? 
-                                    (<>
+                                { invoice.tax === 1 ? (
+                                    <>
                                         <td>{gst}</td>
                                         <td>{total}</td>
-                                    </>) : 
-                                    (<>
+                                    </>
+                                ) : (
+                                    <>
                                         <td>0.00</td>
                                         <td>{cost}</td>
-                                    </>)
+                                    </>
+                                )
                                 }
                                 <td>
                                     <form>
