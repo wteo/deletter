4a3137489433da8fc320b8307565ffe2e5961000
Wendy Teo <wendyteo.wy@gmail.com>
2023-01-01 12:05:20 +1100

    Reorganized files within Demand Letter folder

--- src/components/DemandLetter/DemandLetter.tsx
+++ src/components/DemandLetter/DemandLetter.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import styles from './DemandLetter.module.scss';
 
-import BillingAddress from './Invoices/BillingAddress/BilllingAddress';
+import BillingAddress from './BillingAddress/BillingAddress';
 import Invoices from './Invoices/Invoices';
 
 function DemandLetter() {
