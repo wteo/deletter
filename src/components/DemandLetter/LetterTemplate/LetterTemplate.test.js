9706cb6e7ae1d547e2cd9811c431c0217131762f
Wendy Teo <wendyteo.wy@gmail.com>
2023-01-06 10:34:00 +1100

    Broke down test for each children component

--- src/components/DemandLetter/LetterTemplate/LetterTemplate.test.js
+++ src/components/DemandLetter/LetterTemplate/LetterTemplate.test.js
@@ -3,9 +3,34 @@ import { render, screen } from "@testing-library/react";
 import LetterTemplate from './LetterTemplate';
 
 describe('LetterTemplate Component', () => {
-    test('Renders "Letter Template" as a whole', () => {
+
+    test('Renders "Billing Address" ', () => {
         render(<LetterTemplate/>);
-        const text = screen.getByText(/Re: Demand for Payment of Overdue Invoices/i);
-        expect(text).toBeVisible();
+        const billingAddressTestId = screen.getByTestId('billingAddress');
+        expect(billingAddressTestId).toBeInTheDocument();
+    })
+    
+    test('Renders "Title" as text', () => {
+        render(<LetterTemplate/>);
+        const titleText = screen.getByText(/Re: Demand for Payment of Overdue Invoices/i);
+        expect(titleText).toBeVisible();
+    });
+
+    test('Renders "Invoice Table" ', () => {
+        render(<LetterTemplate/>);
+        const table = screen.getByRole('table');
+        expect(table).toBeVisible();
+    });
+
+    test('Renders "Main Content" as text', () => {
+        render(<LetterTemplate/>);
+        const mainContentText = screen.getByText(/total amount due/i);
+        expect(mainContentText).toBeVisible();
+    });
+
+    test('Renders "Signature" as text', () => {
+        render(<LetterTemplate/>);
+        const sincerelyText = screen.getByText(/Sincerely/i);
+        expect(sincerelyText).toBeVisible();
     });
 })
