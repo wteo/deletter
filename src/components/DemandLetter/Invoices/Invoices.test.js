ddc31179688e9e395939ca0d966ce3c10e960ed7
Wendy Teo <wendyteo.wy@gmail.com>
2023-01-06 09:39:53 +1100

    Added new test

--- src/components/DemandLetter/Invoices/Invoices.test.js
+++ src/components/DemandLetter/Invoices/Invoices.test.js
@@ -1,10 +1,11 @@
 import '@testing-library/jest-dom'
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import userEvent from '@testing-library/user-event';
 import { AuthProvider } from '../../../contexts/AuthContext';
 import { DbProvider } from '../../../contexts/DbContext';
 import Invoices from './Invoices';
 import InvoiceForm from './Form/InvoiceForm';
+
 
 const MockProvider = (props) => {
     return (
@@ -58,6 +59,17 @@ describe('Invoices Component: Form', () => {
         submit();
         expect(await screen.findByText(/Cost must be in positive balance/i)).toBeVisible();
     });
+
+    test('Renders "Error Message" where credit note or overpayment is in positive', async() => {
+        render(<InvoiceForm/>, { wrapper: MockProvider });
+        userInput('docNo', 'INV001');
+        userInput('date', '30-04-20');
+        const selectElement = await screen.findByTestId('docType');
+        userEvent.selectOptions(selectElement, 'Overpayment');
+        userInput('cost', '1000');
+        submit();
+        expect(await screen.findByText(/Cost must be in negative balance/i)).toBeVisible();
+    });
     
     test('Renders "Error Message" where customer name is not selected', async() => {
         render(<InvoiceForm/>, { wrapper: MockProvider });
@@ -67,6 +79,29 @@ describe('Invoices Component: Form', () => {
         submit();
         expect(await screen.findByText(/Please select an existing customer/i)).toBeVisible();
     });
+
+    test('Adding new option to Customer Name selection', () => {
+        render(<Invoices/>, { wrapper: MockProvider });
+        userInput('docNo', 'INV001');
+        userInput('date', '30-04-20');
+        userInput('cost', '1000');
+        
+        // Addding new option to Customer Name Selection
+        const selectEl = screen.getByTestId('customer');
+        const newOption = document.createElement('option');
+        newOption.value = 'ABC Inc';
+        newOption.textContent = 'ABC Inc';
+        selectEl.append(newOption);
+        userEvent.selectOptions(selectEl, 'ABC Inc');
+        expect(selectEl.value).toBe('ABC Inc');
+
+        submit();
+
+        waitFor(async() => expect(await screen.findByText(/INV001/i).toBeVisible()));
+        waitFor(async() => expect(await screen.findByText(/INV001/i).not.toBeVisible())); // Something is wrong here...
+
+    });
+    
 });
 
 describe('Invoices Component: Table', () => {
@@ -75,6 +110,5 @@ describe('Invoices Component: Table', () => {
         render(<Invoices/>, { wrapper: MockProvider });
         expect(await screen.findByRole('table')).toBeVisible();
     });
-
 });
     
