dd40419037058e6a9b71246901c18460c90e54c9
Wendy Teo <wendyteo.wy@gmail.com>
2023-01-07 17:20:32 +1100

    Change date format

--- src/components/DemandLetter/LetterTemplate/TodayDate.tsx
+++ src/components/DemandLetter/LetterTemplate/TodayDate.tsx
@@ -3,9 +3,12 @@ import React from 'react'
 function TodayDate() {
     
     const today = new Date();
-    const year = today.getFullYear();
-    const month = today.toLocaleString('default', { month: 'long' });
-    const day = String(today.getDate()).padStart(2, '0');
+    const todayString = today.toString();
+    const todayArr = todayString.split(' ');    
+    
+    const day: string = todayArr[2];
+    const month: string = todayArr[1];
+    const year: string = todayArr[3];
     const date = `${day} ${month}, ${year}`;
 
     return <p>{date}</p>;
