6038005928a95759f6d73f25a8c50955e6e05e80
Wendy Teo <wendyteo.wy@gmail.com>
2023-01-07 17:20:12 +1100

    Sorted invoices in table as per date

--- src/components/DemandLetter/LetterTemplate/MainContent/InvoiceTable/InvoiceTable.tsx
+++ src/components/DemandLetter/LetterTemplate/MainContent/InvoiceTable/InvoiceTable.tsx
@@ -2,6 +2,13 @@ import React from 'react';
 import NewInvoiceRow from './NewInvoiceRow';
 
 function InvoiceTable(props: { invoices: any }) {
+
+    const sortInvsByDates = props.invoices.sort((a: { date: string }, b: { date: string }) => {
+        const x: string = a.date.split('-').reverse().join();
+        const y: string = b.date.split('-').reverse().join();
+        return x < y ? -1 : (x > y ? 1 : 0);
+        });
+
     return (
         <table>
             <tr>
@@ -10,7 +17,7 @@ function InvoiceTable(props: { invoices: any }) {
                 <th>Document Date</th>
                 <th>Amount</th>
             </tr>
-            { props.invoices.map((invoice: { docNo: string, docType: string, date: string, cost: number, tax: number }) => 
+            { sortInvsByDates.map((invoice: { docNo: string, docType: string, date: string, cost: number, tax: number }) => 
                 <NewInvoiceRow key={invoice.docNo} docNo={invoice.docNo} docType={invoice.docType} date={invoice.date} cost={invoice.cost} tax={invoice.tax} />)
             }
         </table>
