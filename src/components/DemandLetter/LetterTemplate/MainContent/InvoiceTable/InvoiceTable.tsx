4cd5806528daeb060d2c72e2d546b4ce007ca5d5
Wendy Teo <wendyteo.wy@gmail.com>
2023-01-08 05:55:41 +1100

    Broke down the styling into mixin and variables

--- src/components/DemandLetter/LetterTemplate/MainContent/InvoiceTable/InvoiceTable.tsx
+++ src/components/DemandLetter/LetterTemplate/MainContent/InvoiceTable/InvoiceTable.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import NewInvoiceRow from './NewInvoiceRow';
 
+import style from './InvoiceTable.module.scss';
+
 function InvoiceTable(props: { invoices: any }) {
 
     const sortInvsByDates = props.invoices.sort((a: { date: string }, b: { date: string }) => {
@@ -10,16 +12,20 @@ function InvoiceTable(props: { invoices: any }) {
         });
 
     return (
-        <table>
-            <tr>
-                <th>Document No</th>
-                <th>Document Type</th>
-                <th>Document Date</th>
-                <th>Amount</th>
-            </tr>
-            { sortInvsByDates.map((invoice: { docNo: string, docType: string, date: string, cost: number, tax: number }) => 
-                <NewInvoiceRow key={invoice.docNo} docNo={invoice.docNo} docType={invoice.docType} date={invoice.date} cost={invoice.cost} tax={invoice.tax} />)
-            }
+        <table id={style.invoicesListLetter}>
+            <thead>
+                <tr>
+                    <th>Document No</th>
+                    <th>Document Type</th>
+                    <th>Document Date</th>
+                    <th>Amount</th>
+                </tr>
+            </thead>
+            <tbody>
+                { sortInvsByDates.map((invoice: { docNo: string, docType: string, date: string, cost: number, tax: number }) => 
+                    <NewInvoiceRow key={invoice.docNo} docNo={invoice.docNo} docType={invoice.docType} date={invoice.date} cost={invoice.cost} tax={invoice.tax} />)
+                }
+            </tbody>
         </table>
     );
 }
