ba45c199cb18f21eac59fff5d522be7355cba3d8
Wendy Teo <wendyteo.wy@gmail.com>
2023-01-07 17:21:17 +1100

    Added func to update billed date to due date

--- src/components/DemandLetter/LetterTemplate/MainContent/MainContent.tsx
+++ src/components/DemandLetter/LetterTemplate/MainContent/MainContent.tsx
@@ -28,6 +28,36 @@ function MainContent(props: { recipient: string, invoices: any }) {
     // Formatting nums
     const formatNum = (num: number) => num.toLocaleString(undefined, { minimumFractionDigits: 2, maximumFractionDigits: 2 });
 
+    // Sorting dates from oldest to newest
+    const docDates: string[] = props.invoices.map((invoice: invoice) => invoice.date);
+    const sortedDocDates = docDates.sort(function(a, b){
+        const x: string = a.split('-').reverse().join();
+        const y: string = b.split('-').reverse().join();
+        return x < y ? -1 : (x > y ? 1 : 0);
+        });
+
+    const paymentTerm: number = 30;
+    let oldestDueDate: string = '';
+
+    if (docDates.length > 0) {
+        
+        const docDateArr = sortedDocDates[0].split('-');
+        const [day, month, year ]: string[] = docDateArr;
+        const oldestDocDate = new Date(String(`${year}-${month}-${day}`));
+        
+        const updatedDate = oldestDocDate.setDate(oldestDocDate.getDate() + Number(paymentTerm));
+        const dueDate = new Date(updatedDate);
+        const dueDateString = dueDate.toString();
+        const dueDateArr = dueDateString.split(' ');
+        
+        const updatedDay: string = dueDateArr[2];
+        const updatedMonth: string = dueDateArr[1];
+        const updatedYear: string = dueDateArr[3];
+        const formattedDueDate: string = `${updatedDay} ${updatedMonth}, ${updatedYear}`;
+        
+        oldestDueDate = formattedDueDate;
+    }
+
     return (
         <div>
             <p>Dear {firstName === '' || firstName.includes('Billed') ? 'Customer' : firstName},</p>
@@ -35,9 +65,11 @@ function MainContent(props: { recipient: string, invoices: any }) {
             { props.invoices.length < 1  && noDocErrMessage }
             { sum < 0 && creditErrMessage }
             { props.invoices.length >= 1 && <InvoiceTable invoices={props.invoices} />}
-            <p>The total amount due is ${ props.invoices.length >= 1 ? formatNum(sum) : '[Total Amount Due]'}. These invoices have been overdue since [most Overdue Invoice].</p>
+            <p>The total amount due is ${ props.invoices.length >= 1 ? formatNum(sum) : '[Total Amount Due]'}, with the  
+            oldest invoice overdue since {props.invoices.length >= 1 ? oldestDueDate : '[most Overdue Invoice]'}.
+            </p>
             <p>I understand that circumstances may arise that prevent timely payment, and I am willing to work with you to find a solution. 
-                However, under our agreement, you are required to pay these documents in a timely manner. Despite our attempt to contact
+                However, under our agreement, you are required to pay these documents within the {paymentTerm} days payment term. Despite our attempt to contact
                 you on multiple occasions, these invoices have remained outstanding.
             </p>
             <p>Therefore, I request that you arrange payment within 14 days from the date of this letter. 
